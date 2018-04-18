e1317625ab6e5b624e444196d592bbe56356f21d
Maksim Chayun <maksimchayun1@Maksims-iMac-7.local>
2018-04-18 19:11:57 -0400

    Main
    added deck

--- main.cpp
+++ main.cpp
@@ -8,13 +8,40 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 #include "Card.h"
 
 using namespace std;
 
 
 int main() {
-    Card thisc(2,11); // Enter a number for a suit, and an order number. Try it
-    cout << thisc;
+    vector<Card*> deck;
+    Card* deckptr = NULL;
+    for(int i = 1; i < 5; i++){
+        for(int j = 1; j < 14 ; j++){
+            deckptr = new Card(i,j);
+            deck.push_back(deckptr);
+            cout << endl << *deckptr << endl;
+        }
+    }
+    
+    cout << deck.size() << endl;
+    
+    /* random_shuffle(deck.begin(),deck.end());
+    
+    for (int k = 0; k < deck.size(); k++)
+    {
+        cout << endl << (*(deck[k])) << endl;
+    }
+    */
+    
+    for (int k = 0; k < deck.size(); k++)
+    {
+        delete deck[k];
+    }
+    
+    //Card thisc(2,11); // Enter a number for a suit, and an order number. Try it
+    //cout << thisc;
     return 0;
 }
