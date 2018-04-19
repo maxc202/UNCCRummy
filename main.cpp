ff033990b7621a12f93e05149f34dbac2a088e51
maxc202 <38508799+maxc202@users.noreply.github.com>
2018-04-18 20:03:44 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,3 @@
-//
-//  main.cpp
-//  Stuff
-//
-//  Created by Maksim Chayun on 4/17/18.
-//  Copyright © 2018 Maksim Chayun. All rights reserved.
-//
 
 #include <iostream>
 #include <string>
@@ -14,34 +7,37 @@
 
 using namespace std;
 
+vector<Card*> makedeck();  //Makes a deck of cards
 
 int main() {
-    vector<Card*> deck;
-    Card* deckptr = NULL;
-    for(int i = 1; i < 5; i++){
-        for(int j = 1; j < 14 ; j++){
-            deckptr = new Card(i,j);
-            deck.push_back(deckptr);
-            cout << endl << *deckptr << endl;
-        }
-    }
-    
-    cout << deck.size() << endl;
-    
-    /* random_shuffle(deck.begin(),deck.end());
+    vector<Card*> deck = makedeck();
     
     for (int k = 0; k < deck.size(); k++)
     {
         cout << endl << (*(deck[k])) << endl;
     }
-    */
     
     for (int k = 0; k < deck.size(); k++)
     {
         delete deck[k];
     }
     
-    //Card thisc(2,11); // Enter a number for a suit, and an order number. Try it
-    //cout << thisc;
     return 0;
 }
+
+vector<Card*> makedeck()
+{
+    vector<Card*> playdeck;
+    Card* deckptr = NULL;
+    for(int i = 1; i < 5; i++){
+        for(int j = 1; j < 14 ; j++){
+            deckptr = new Card(i,j);
+            playdeck.push_back(deckptr);
+            cout << endl << *deckptr << endl;
+        }
+    }
+    random_shuffle(playdeck.begin(),playdeck.end());
+    return playdeck;
+}
+
+
