7c1fbe829f2d9308a8b91d685547fd7abc9c82ad
Ryan <38287035+rbarbouo6@users.noreply.github.com>
2018-04-19 21:16:45 -0400

    Update Deck.h

--- Deck.h
+++ Deck.h
@@ -10,12 +10,21 @@ private:
   Card* deck[52];
 public:
   Deck(){
+    int count = 0;
     for(int i = 0; i < 13; i++){
-      for(int j = 0; j < 4; j++
-
-
-
-
-
-
+      for(int j = 0; j < 4; j++){
+        Card temp = new Card(i, j);
+        deck[count] = temp;
+        delete temp;
+        count++;
+      }
+    }
+  }
+  
+  void output(){
+    for(int i = 0; i < 52; i++){
+      deck[i]->output();
+      cout << endl;
+    }
+  }
 };
