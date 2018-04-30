fee632a4f1c57a89df064d8518c04bc4f3caf251
maxc202 <38508799+maxc202@users.noreply.github.com>
2018-04-30 00:48:22 -0400

    Update Hand.h

--- Hand.h
+++ Hand.h
@@ -65,4 +65,68 @@ public:
     }
     return deadwood;
   }
+          
+void findSets()
+{
+    sort(hand.begin,hand.end, dumbsort);
+    for(int k = 1; k != (hand.size() - 1); k++)
+    {
+        if(
+           (abs((( hand[k].getOrder()-( hand[k-1]getOrder() )))==1) && abs(((hand[k].getOrder())-(hand[k+1]getOrder()))) == 1)
+           )
+        {
+            if((k+2)<=hand.sizeof() && hand[k].getOrder()==hand[k+2].getOrder())
+            {
+                sets.push_back(hand[k+2]);
+                hand.erase(k+2);
+                
+            }
+            sets.push_back(hand[k-1]);
+            sets.push_back(hand[k]);
+            sets.push_back(hand[k+1]);
+            hand.erase(k);
+            hand.erase(k-1);
+            hand.erase(k+1);
+            
+            k=0;
+        }
+        }
+}
+    
+                        
+void findRuns()
+{
+    sortHand();
+    for(int k = 0; k != (hand.sizeof - 1); k++)
+    {
+        int curr = k;
+        int count = 1;
+        while((hand[curr+1].getOrder() == hand[curr].getOrder+1) && (hand[curr].getSuit == hand[curr+1].getSuit)
+        {
+            count++;
+        }
+        if(count >= 3){
+            for(int u = k + count-1; u >= k; u--)
+            {
+                runs.push_back(hand[u]);
+                hand.erase(u);
+            }
+            k=0;
+    }
+        /*
+        if(hand[k].getSuit==hand[k-1].getSuit && hand[k].getSuit==hand[k+1].getSuit)
+        {
+            runs.push_back(hand[k-1]);
+            runs.push_back(hand[k]);
+            runs.push_back(hand[k+1]);
+            hand.erase(k);
+            hand.erase(k-1);
+            hand.erase(k+1);
+        }
+         */
+        
+    }
+}                   
+          
+          
 };
