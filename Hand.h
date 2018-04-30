a0ab496ccf0e068e8c830f56c1ac517350a95673
Ryan <38287035+rbarbouo6@users.noreply.github.com>
2018-04-30 16:04:00 -0400

    Update Hand.h

--- Hand.h
+++ Hand.h
@@ -1,21 +1,21 @@
 #ifndef HAND_H
-	#define HAND_H
+#define HAND_H
 	
 
-	#include <iostream>
-	#include <vector>
-	#include <algorithm>
-	#include "Card.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "Card.h"
 	
 
-	using namespace std;
-	bool way2sort(Card* i, Card* j) {return *i < *j;}
-	bool dumbsort(Card* i, Card* j) {return *i > *j;}
+using namespace std;
+bool way2sort(Card* i, Card* j) {return *i < *j;}
+bool dumbsort(Card* i, Card* j) {return *i > *j;}
 	
 
 	
 
-	class Hand{
+class Hand{
 	private:
 	  vector<Card*> hand;
 	  vector<Card*> runs;
@@ -25,14 +25,12 @@
 	  void draw(Card* a){
 	    hand.push_back(a);
 	  }
-	   
-	void sortHand()
-	{
+	  
+	void sortHand(){
 	    sort(hand.begin(), hand.end() , way2sort);
 	}
 	            
 	void sortRuns(){
-	    
 	    sort(runs.begin(), runs.end(), way2sort);
 	}
 	            
@@ -67,18 +65,13 @@
 	          
 	void findSets()
 	{
-	    sort(hand.begin,hand.end, dumbsort);
-	    for(int k = 1; k != (hand.size() - 1); k++)
-	    {
-	        if(
-	           (abs((( hand[k]->getOrder()-( hand[k-1]getOrder() )))==1) && abs(((hand[k]->getOrder())-(hand[k+1]getOrder()))) == 1)
-	           )
+	    sort(hand.begin(),hand.end(), dumbsort);
+	    for(int k = 1; k != (hand.size() - 1); k++){
+	        if((abs((( hand[k]->getOrder()-( hand[k-1]->getOrder() )))==1) && abs(((hand[k]->getOrder())-(hand[k+1]->getOrder()))) == 1))
 	        {
-	            if((k+2)<=hand.sizeof() && hand[k]->getOrder()==hand[k+2]->getOrder())
-	            {
+	            if((k+2)<=hand.size() && hand[k]->getOrder()==hand[k+2]->getOrder()){
 	                sets.push_back(hand[k+2]);
-	                hand.erase(hands.begin()+k+2);
-	                
+	                hand.erase(hands.begin()+k+2);      
 	            }
 	            sets.push_back(hand[k-1]);
 	            sets.push_back(hand[k]);
@@ -89,46 +82,27 @@
 	            
 	            k=0;
 	        }
-	        }
+	    }
 	}
 	    
 	                        
-	void findRuns()
-	{
+	void findRuns(){
 	    sortHand();
-	    for(int k = 0; k != (hand.sizeof - 1); k++)
-	    {
+	    for(int k = 0; k != (hand.sizeof - 1); k++){
 	        int curr = k;
 	        int count = 1;
-	        while((hand[curr+1]->getOrder() == hand[curr]->getOrder()+1) && (hand[curr]->getSuit() == hand[curr+1]->getSuit())
-	        {
+	        while((hand[curr+1]->getOrder() == hand[curr]->getOrder()+1) && (hand[curr]->getSuit() == hand[curr+1]->getSuit()){
 	            count++;
 	        }
 	        if(count >= 3){
-	            for(int u = k + count-1; u >= k; u--)
-	            {
+	            for(int u = k + count-1; u >= k; u--){
 	                runs.push_back(hand[u]);
 	                hand.erase(hands.begin()+u);
 	            }
-	            k=0;
-	    }
-	        /*
-	        if(hand[k]->getSuit==hand[k-1]->getSuit && hand[k]->getSuit==hand[k+1]->getSuit)
-	        {
-	            runs.push_back(hand[k-1]);
-	            runs.push_back(hand[k]);
-	            runs.push_back(hand[k+1]);
-	            hand.erase(k);
-	            hand.erase(k-1);
-	            hand.erase(k+1);
-	        }
-	         */
-	        
-	    }
-	}                   
-	          
-	          
-	};
+	        k=0;
+		}
+	}
+};
 	              
 	              
 	              
